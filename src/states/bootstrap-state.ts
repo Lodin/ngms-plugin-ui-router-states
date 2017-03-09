33dddf7c181f260212305cdcd0cbb5bc93b9e51d
Vlad Rindevich <rindevich.vs@gmail.com>
2017-03-09 22:59:41 +0300

    refactor(states): move component initialization from bootstrap-state function

--- src/states/bootstrap-state.ts
+++ src/states/bootstrap-state.ts
@@ -1,10 +1,28 @@
 import {ModuleMetadata, tokens as ngmsTokens} from 'ng-metasys';
 import * as tokens from '../core/tokens';
+import bootstrapHooks from '../hooks/bootstrap-hooks';
 import {Hooks} from '../hooks/hooks';
 import {StateDeclaration} from './state-declaration';
 import applyStates from './apply-states';
 import checkComponent from './check-component';
-import bootstrapHooks from '../hooks/bootstrap-hooks';
+
+type InitComponent = (declaration: any) => [StateDeclaration, Hooks|null];
+const initComponent: InitComponent =
+  declaration => {
+    checkComponent(declaration);
+
+    const state: StateDeclaration = Reflect.getMetadata(tokens.state, declaration.prototype);
+
+    if (!state.name) {
+      throw new Error(`State name for ${declaration.name} is not set`);
+    }
+
+    state.component = declaration;
+
+    const hooks = bootstrapHooks(state.component) as Hooks|null;
+
+    return [state, hooks];
+  };
 
 type BootstrapState = (ngModule: angular.IModule, declaration: any) => void;
 const bootstrapState: BootstrapState =
@@ -19,28 +37,22 @@ const bootstrapState: BootstrapState =
     const states: StateDeclaration[] = [];
     const hooks = new Map<any, Hooks>();
 
-    for (const possibleStateOwner of moduleMetadata.declarations) {
-      if (!Reflect.hasMetadata(tokens.state, possibleStateOwner.prototype)) {
+    for (const component of moduleMetadata.declarations) {
+      if (!Reflect.hasMetadata(tokens.state, component.prototype)) {
         continue;
       }
 
-      const state: StateDeclaration =
-        Reflect.getMetadata(tokens.state, possibleStateOwner.prototype);
-
-      if (!state.name) {
-        throw new Error(`State name for ${possibleStateOwner.name} is not set`);
-      }
+      const [state, componentHooks] = initComponent(component);
 
       states.push(state);
 
-      if (state.component) {
-        checkComponent(state.component);
-        const componentHooks = bootstrapHooks(state.component) as Hooks|null;
-
-        if (componentHooks) {
-          hooks.set(state.component, componentHooks);
-        }
+      if (componentHooks) {
+        hooks.set(state.component, componentHooks);
       }
+    }
+
+    if (states.length === 0) {
+      return;
     }
 
     applyStates(ngModule, states, hooks);
