b14fc9ac155cde55c991e415785b4735d18545d3
Vlad Rindevich <rindevich.vs@gmail.com>
2017-03-12 21:25:37 +0300

    feat: add plugin registration and api exporting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 import 'reflect-metadata';
+import {registerPlugin} from 'ng-metasys';
+import * as tokens from './core/tokens';
+import bootstrapState from './states/bootstrap-state';
+import bootstrapStates from './states/bootstrap-states';
+
+registerPlugin(bootstrapState);
+registerPlugin(bootstrapStates);
+
+export {tokens};
+export {hasStates, isState} from './core/reflection';
+export {default as State} from './states/state-decorator';
+export {default as States} from './states/states-decorator';
